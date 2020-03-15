45b4b43d5c52bff96efd0b543de739f14f0e1e0b
Nazar.M <inazar.m@gmail.com>
2020-03-15 20:34:58 +0500

    login with database successfully

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" a href="css\style.css">
+	<link rel="stylesheet" a href="css\font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+	<img src="img/default-profile.jpg"/>
+		<form method="POST" action="login.php">
+			<div class="form-input">
+				<input type="text" name="username" placeholder="Enter the User Name"/>	
+			</div>
+			<div class="form-input">
+				<input type="password" name="password" placeholder="password"/>
+			</div>
+			<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+		</form>
+	</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php 
+ include("connection.php");
+
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from tblusers where Email='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=mysqli_query($con,$sql);
+    
+    if(mysqli_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    } 
+?>
